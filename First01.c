db61c81aef49f0a3589f5c210d8df63c1c62e2f2
Raghav Sharma <Raghav.28524@stu.upes.ac.in>
2025-08-31 12:50:20 +0530

    Update First01.c
    This code adds two numbers given by the user. A basic mathematical operation.

--- First01.c
+++ First01.c
@@ -1,6 +1,18 @@
-// 🖥️
-// Q1 (User Inputs, Operations & Output)
-// Write a program to input two numbers and display their sum.
+// Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
 #include<stdio.h>
 int main(){
   int a,b,sum ;
