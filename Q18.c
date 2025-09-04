4f427479892859ada3611437dfa6a8f24018d1bd
Raghav Sharma <Raghav.28524@stu.upes.ac.in>
2025-09-04 11:03:48 +0530

    Update Q18.c

--- Q18.c
+++ Q18.c
@@ -1,4 +1,9 @@
-Q18: Write a program to assign grades based on a percentage input.
+Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.
 
 /*
 Sample Test Cases:
@@ -40,9 +45,6 @@ int main(){
   }
   else if(grade<70 && grade>=60){
     printf("Grade D");
-  }
-  else if(grade<60 && grade>50){
-    printf("Grade E");
   }
   else {
     printf("Grade F");
